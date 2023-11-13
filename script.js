6c7259ec71906d816999fc6f01ea51e48dccb4dc
Fernando Adolfo <67721157+Ferthronix224@users.noreply.github.com>
2023-11-12 22:52:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,37 @@ function updateCountdown() {
         const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-        countdownElement.textContent = `${days} días, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
+        if(days == 1){
+            countdownElement.textContent = `${days} día, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
+        }else if(hours == 1){
+            countdownElement.textContent = `${days} días, ${hours} hora, ${minutes} minutos, ${seconds} segundos`;
+        }else if(minutes == 1){
+            countdownElement.textContent = `${days} días, ${hours} horas, ${minutes} minuto, ${seconds} segundos`;
+        }else if(seconds == 1){
+            countdownElement.textContent = `${days} días, ${hours} horas, ${minutes} minutos, ${seconds} segundo`;
+        }else if(days == 1 && hours == 1){
+            countdownElement.textContent = `${days} día, ${hours} hora, ${minutes} minutos, ${seconds} segundos`;
+        }else if(days == 1 && minutes == 1){
+            countdownElement.textContent = `${days} día, ${hours} horas, ${minutes} minuto, ${seconds} segundos`;
+        }else if(days == 1 && seconds == 1){
+            countdownElement.textContent = `${days} día, ${hours} horas, ${minutes} minutos, ${seconds} segundo`;
+        }else if(hours == 1 && minutes == 1){
+            countdownElement.textContent = `${days} días, ${hours} hora, ${minutes} minuto, ${seconds} segundos`;
+        }else if(hours == 1 && seconds == 1){
+            countdownElement.textContent = `${days} días, ${hours} hora, ${minutes} minutos, ${seconds} segundo`;
+        }else if(minutes == 1 && seconds == 1){
+            countdownElement.textContent = `${days} días, ${hours} horas, ${minutes} minuto, ${seconds} segundo`;
+        }else if(days == 1 && hours == 1 && minutes == 1){
+            countdownElement.textContent = `${days} día, ${hours} hora, ${minutes} minuto, ${seconds} segundos`;
+        }else if(days == 1 && hours == 1 && seconds == 1){
+            countdownElement.textContent = `${days} día, ${hours} hora, ${minutes} minutos, ${seconds} segundo`;
+        }else if(hours == 1 && minutes == 1 && seconds == 1){
+            countdownElement.textContent = `${days} días, ${hours} hora, ${minutes} minuto, ${seconds} segundo`;
+        }else if(days == 1 && minutes == 1 && seconds == 1){
+            countdownElement.textContent = `${days} día, ${hours} horas, ${minutes} minuto, ${seconds} segundo`;
+        }else if(days == 1 && hours == 1 && minutes == 1 && seconds == 1){
+            countdownElement.textContent = `${days} día, ${hours} hora, ${minutes} minuto, ${seconds} segundo`;
+        }
     }
 }
 
