6e4b687be57a04359e1ed57a2e238a238cc5691c
Fernando Adolfo <fernando.adolfo.002@gmail.com>
2024-09-02 22:49:44 -0600

    Exito

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const countdownInterval = setInterval(updateCountdown, 1000);
 const countdownElement = document.getElementById('countdown');
 
 function updateCountdown() {
-    const currentDate = new Date();
-    const selected_date = new Date(`${current_date.getFullYear()}-05-20.`)
-    
-    if (selected_date - current_date <= 0) {
-        selected_date.setFullYear(current_date.getFullYear() + 1)
-    }
-    const timeDifference = selectedDate - currentDate;
+    const current_date = new Date()  
+    const selected_date = new Date(`${current_date.getFullYear()}-09-02.`)
 
-    if (`${current_date.getDay()}-${current_date.getMonth()}-${current_date.getFullYear()}` === `${selected_date.getDay()}-${selected_date.getMonth()}-${selected_date.getFullYear()}`) {
-        clearInterval(countdownInterval);
+    const current_date_str = current_date.toDateString()
+    const selected_date_str = selected_date.toDateString()
+    
+    let time_difference = selected_date - current_date;
+    
+    if (time_difference <= 0) {
+        selected_date.setFullYear(current_date.getFullYear() + 1)     
+        time_difference = selected_date - current_date;
+    }    
+    
+    if (current_date_str == selected_date_str) {
         countdownElement.textContent = '¡Hora de hacer desmadre!';
     } else {
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const days = Math.floor(time_difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((time_difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((time_difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((time_difference % (1000 * 60)) / 1000);
 
         if (days == 1 && hours == 1 && minutes == 1 && seconds == 1) {
             countdownElement.textContent = `${days} día, ${hours} hora, ${minutes} minuto, ${seconds} segundo`;
@@ -52,6 +56,5 @@ function updateCountdown() {
         } else{
             countdownElement.textContent = `${days} días, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
         }
-
     }
 }
